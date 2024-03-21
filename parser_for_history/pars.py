dad80b130fc936ed191452cabd548ebae719910c
Wester35 <shum0302@gmail.com>
2024-03-21 23:16:18 +0300

    realisation json files

--- parser_for_history/pars.py
+++ parser_for_history/pars.py
@@ -6,6 +6,8 @@ from fake_useragent import UserAgent
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
+import json
+
 
 date_now = datetime.date.today()
 date_now = str(date_now)[:7]
@@ -13,6 +15,7 @@ path = f'images/{date_now}.jpg'
 url_for_image = 'https://www.hramalnevskogo.ru/page40966859.html'
 url_to_parse = 'https://www.hramalnevskogo.ru/page41137881.html'
 list_for_urls = []
+
 
 
 def get_fake_user_agent():
@@ -67,14 +70,48 @@ async def parse_html(url):
 
         print(list_for_urls)
 
-        # pars everyone urls###########################################################
+        # pars every urls###########################################################
         for _url in list_for_urls:
             driver.get(_url)
             filename = _url.split('/')[-1]
             _soup = BeautifulSoup(driver.page_source, 'html.parser')
             div_object = _soup.find('div', class_='t-feed__post-popup__content t-col t-col_8')
-            with open(f'webpages/{filename}.html', "w", encoding='utf-8') as file:
-                file.write(str(div_object))
+            div_image = div_object.find_all('div', class_='t-slds__bgimg t-bgimg t-slds__bgimg-contain')
+            video = div_object.find_all('iframe')
+
+            image_links = [div['data-original'] for div in div_image]
+            video_links = []
+
+            for iframe in video:
+                src = iframe['src']
+                video_links.append(src)
+
+            title = _soup.find('h1', class_='js-feed-post-title t-feed__post-popup__title t-title t-title_xxs')
+            history_date = _soup.find('span', class_='js-feed-post-date t-feed__post-popup__date t-uptitle t-uptitle_sm')
+
+            div_history_text = _soup.find('div', class_='t-redactor__tte-view')
+            #his_list = []
+            if div_history_text:
+                history_text = div_history_text.find('div', class_='t-redactor__text')
+                paragraphs = []
+                for p in history_text.find_all_next(string=True):
+                    stripped_p = p.strip()
+                    if stripped_p:
+                        paragraphs.append(stripped_p)
+            else:
+                paragraphs = []
+            if paragraphs:
+                paragraphs = paragraphs[:-14]
+            data = {
+                "title": str(title.get_text()),
+                "history_date": str(history_date.get_text()),
+                "images": image_links,
+                "videos": video_links,
+                "history_text": paragraphs
+            }
+            await asyncio.sleep(0.5)
+            with open(f'webpages/{filename}.json', "w", encoding='utf-8') as file:
+                json.dump(data, file, ensure_ascii=False, indent=4)
 
     except Exception as e:
         print(f"Error parsing HTML: {e}")
